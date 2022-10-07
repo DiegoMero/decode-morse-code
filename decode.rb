fd8002d1c4bc378618c7625c9c83c8bd4d2689b4
DiegoMero <diegomero_1999@hotmail.com>
2022-10-07 04:07:39 -0500

    "Add get_letter and get_word methods"

---
+++ decode.rb
@@ -0,0 +1,39 @@
+@letters = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def get_letter (morse_character)
+  @letter = @letters[morse_character]
+end
+
+def get_word (morse_word)
+  @word = morse_word.split
+  @word = @word.map do |element|
+    get_letter(element)
+  end
+end
